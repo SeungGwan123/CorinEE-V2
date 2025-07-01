fa4b49d2ae2b20400c20d22e7d12f70bd70977f9
SeungGwa123 <skwan123@naver.com>
2025-07-01 21:16:43 +0900

    test: nest 서버 로컬 호스트 바인딩 문제 테스트
    export 포트 문제인지 로컬호스트 바인딩 문제인지 체크를 위한 배포 테스트

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -40,6 +40,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(process.env.PORT ?? 3002,'0.0.0.0');
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
